c4dfbbeb9925b267ddea119b4a56e75de69d6a9d
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-14 03:11:24 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,22 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    discriminante = (b ** 2 - (4 * a * c)) ** 0.5
+    x1 = (-b + discriminante) / (2 * a)
+    x2 = (-b - discriminante) / (2 * a)
+    return x1, x2
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a * (x ** 2) + b * x + c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    return f"f(x) = {a}x^2 + {b}x + {c}"
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    derivada_a = 2 * a
+    derivada_b = b
+    return f"f'(x) = {derivada_a}x + {derivada_b}"
