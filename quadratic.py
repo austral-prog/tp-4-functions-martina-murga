53a68841e47b468a1a37c2a24130fae9c6a4c393
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-14 20:36:33 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,10 +1,14 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    discriminante = (b ** 2 - (4 * a * c)) ** 0.5
-    x1 = (-b + discriminante) / (2 * a)
-    x2 = (-b - discriminante) / (2 * a)
-    return x1, x2
+    discriminant_val = b ** 2 - (4 * a * c)
+    if discriminant_val < 0:
+        return "( )"
+
+    discriminant = discriminant_val ** 0.5
+    x1 = (-b + discriminant) / (2 * a)
+    x2 = (-b - discriminant) / (2 * a)
+    return (max(x1, x2), min(x1, x2))
 
 
 def value_y(a, b, c, x):
@@ -13,10 +17,16 @@ def value_y(a, b, c, x):
 
 
 def to_string(a, b, c):
-    return f"f(x) = {a}x^2 + {b}x + {c}"
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 
 def derivation(a, b, c):
     derivada_a = 2 * a
     derivada_b = b
-    return f"f'(x) = {derivada_a}x + {derivada_b}"
+    if derivada_a == 0 and derivada_b == 0:
+        return "f'(x) = 0"
+    if derivada_a == 0:
+        return f"f'(x) = {derivada_b}"
+    if derivada_b == 0:
+        return f"f'(x) = {derivada_a} * X"
+    return f"f'(x) = {derivada_a} * X + {derivada_b}"
