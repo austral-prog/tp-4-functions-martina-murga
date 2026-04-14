e859bb2611c736efdbdadc2d893d783a3542e7d8
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-14 20:43:35 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,24 +1,29 @@
 # Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
     discriminant_val = b ** 2 - (4 * a * c)
     if discriminant_val < 0:
         return "( )"
-
     discriminant = discriminant_val ** 0.5
     x1 = (-b + discriminant) / (2 * a)
     x2 = (-b - discriminant) / (2 * a)
-    return (max(x1, x2), min(x1, x2))
-
+    if discriminant_val == 0:
+        return f"({x1})"
+    mayor = max(x1, x2)
+    menor = min(x1, x2)
+    return f"({mayor}, {menor})"
 
 def value_y(a, b, c, x):
     y = a * (x ** 2) + b * x + c
     return y
 
-
 def to_string(a, b, c):
+    if a == 0 and b == 0:
+        return f"f(x) = {c}"
+    if a == 0:
+        return f"f(x) = {b} * X + {c}"
+    if b == 0:
+        return f"f(x) = {a} * X^2 + {c}"
     return f"f(x) = {a} * X^2 + {b} * X + {c}"
-
 
 def derivation(a, b, c):
     derivada_a = 2 * a
